4323f90cc4cfa242f9c040b2eb91395118052fad
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2024-05-01 07:38:25 -0700

    update readme and mock

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-# Sample Hardhat Project
+# Read and API3 dAPI Price feed to your contract
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
+This project covers two topics.
+- How to get your smart contract to read a price feed from a proxy available from  https://market.api3.org/dapis
+- How to deploy an adaptor to utilize API3 dAPIs without having to refactor your code if you use another library.
 
-Try running some of the following tasks:
+### Files
+
+- `API3PriceFeed.sol` - Base contract setup to receive an API3 price feed
+- `OtherOracle.sol` - Simple contract that receives other popular oracles in their native format
+- `Adaptor.sol` - The adaptor contract the converts the API3 price feed data to match the format that is required by the other oracle contract.
+
+- `Mock Folders` - Contracts that just return fake data in API3 oracle format and AggregatorV3 format
+
+### How to Execute a Price Feed Read
+
+Deploy the API3PriceFeed.sol contract.  Once deployed, you want to choose the price feed you want to read in your contract by going to https://market.api3.org/dapis
+
+Call the `setProxyAddress` with the address for the price feed.  You can then read the data by calling `readDataFeed`
+
+### How to use the adaptor
+
+Deploy 
+
+
+
 
 ```shell
 npx hardhat help
--- test/Test.js
+++ test/Test.js
@@ -31,11 +31,19 @@ describe("dAPI", function () {
     //Set our mock Proxy address that will return static values
     await api3oracle.setProxyAddress(mockDapi.getAddress());
 
+    const MockOtherOracle = await ethers.getContractFactory("MockAggregatorV3");
+    const mockOtherOracle = await MockOtherOracle.deploy();
+    await mockOtherOracle.waitForDeployment();
+
     const OtherOracle = await ethers.getContractFactory("DataConsumerV3");
-    const otherOracle = await OtherOracle.deploy();
+    const otherOracle = await OtherOracle.deploy(mockOtherOracle.getAddress());
     await otherOracle.waitForDeployment();
 
-    return { api3oracle, otherOracle, owner, otherAccount };
+    const Adaptor = await ethers.getContractFactory("Api3AggregatorAdaptor");
+    const adaptor = await Adaptor.deploy(mockDapi.getAddress());
+    await adaptor.waitForDeployment();
+
+    return { api3oracle, otherOracle, adaptor, mockDapi, owner, otherAccount };
   }
 
   describe("Deployment", function () {
@@ -59,52 +67,24 @@ describe("dAPI", function () {
     });
   });
 
-  xdescribe("Deposit", function () {
-    it("Deposit Eth", async function () {
-      const { api3oracle, owner } = await loadFixture(deployBefore);
+  describe("Other Oracle", function () {
+    it("Read Other Oracle", async function () {
+      const { api3oracle, otherOracle, owner } = await loadFixture(deployBefore);
+        let { roundId, answer, startedAt, timestamp, answeredInRound } = await otherOracle.getLatestAnswer();
+        // console.log("Round ID: ", roundId);
+        console.log("Answer: ", answer.toString());
+    });
 
-      let [price, time] = await api3oracle.readDataFeed();
-      //verify our mock values are set
-      // console.log("Price: ", price.toString());
-      // console.log("Time: ", time.toString());
-      //Deposit 1 ETH to recieve tokens
-      await api3oracle.depositCollateral({ value: ethers.parseEther("1") });
-      //Make sure we get our tokens in return **NOTE: This is hardcoded to 1000 tokens per ETH for this example**
-      await expect(await api3oracle.balanceOf(owner.address)).to.be.equal(
-        ethers.parseEther("1000")
-      );
-
-      //Make sure we can't deposit 0 ETH                               //CUSTOM ERRORS GET CHECKED LIKE THIS (contract instance, custom error name)
-      await expect(
-        api3oracle.depositCollateral({ value: ethers.parseEther("0") })
-      ).to.be.revertedWithCustomError(api3oracle, "NoValue");
+    it("Reads from API3 Oracle through adapter", async function () {
+        const { api3oracle, otherOracle, adaptor, mockDapi, owner } = await loadFixture(deployBefore);
+        // await adaptor.setDapiValues(mockDapi.getAddress());
+        await otherOracle.updateOracleSource(adaptor.getAddress());
+        let { roundId, answer, startedAt, timestamp, answeredInRound } = await otherOracle.getLatestAnswer();
+        // console.log("Round ID: ", roundId);
+        console.log("Answer: ", answer.toString());
+        // console.log("Timestamp: ", timestamp.toString());
+        console.log("Answered in Round: ", answeredInRound.toString());
     });
   });
-
-  xdescribe("Reclaim ETH", function () {
-    it("Reclaim Collateral", async function () {
-      const { api3oracle, owner } = await loadFixture(deployBefore);
-      // console logging the eth balances of the owner (not including account abstraction here)
-      //  console.log("Balance: ", await ethers.provider.getBalance(owner.address));
-
-      // First deposit to recieve tokens
-      await api3oracle.depositCollateral({ value: ethers.parseEther("1") });
-      //   Our balance of ETH should be 1 ETH less now
-      //   console.log("Balance: ", await ethers.provider.getBalance(owner.address));
-
-      // Because we hardcoded the price, I'm approving 1000 tokens specifically
-      await api3oracle.approve(api3oracle.getAddress(), ethers.parseEther("1000"));
-      //Reclaim our ETH
-      await api3oracle.reclaimEth(ethers.parseEther("1000"));
-      //   console.log("Balance: ", await ethers.provider.getBalance(owner.address));
-
-      // Try to spend more than I got
-      await api3oracle.approve(api3oracle.getAddress(), ethers.parseEther("1000"));
-      await expect(
-        api3oracle.reclaimEth(ethers.parseEther("1000"))
-      ).to.be.revertedWith("ERC20: burn amount exceeds balance");
-    });
-  });
-
   
 });
